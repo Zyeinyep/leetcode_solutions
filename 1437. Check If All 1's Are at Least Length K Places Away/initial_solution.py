a85b874b4ee71b32d46cae8b0df79ebe652781fe
Zyeinyep Khaval <89563758+Zyeinyep@users.noreply.github.com>
2025-11-17 22:40:28 -0500

    Create initial_solution.py

---
+++ 1437. Check If All 1's Are at Least Length K Places Away/initial_solution.py
@@ -0,0 +1,15 @@
+# The function collects the indices of all 1s, then compares consecutive positions to ensure the gap is > k.
+# If any pair of 1s is too close, it returns False; otherwise, it returns True.
+
+class Solution:
+    def kLengthApart(self, nums: List[int], k: int) -> bool:
+        dict = []
+        for i, e in enumerate(nums):
+            if e == 1:
+                dict.append(i)
+        
+        for i, e in enumerate(dict):
+            if i + 1 < len(dict):
+                if e + k >= dict[i+1]:
+                    return False
+        return True
