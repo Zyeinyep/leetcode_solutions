40d64789cd163865f5478623220246203a108d86
Zyeinyep Khaval <89563758+Zyeinyep@users.noreply.github.com>
2025-11-24 23:17:16 -0500

    Create solution.py

---
+++ 69. Sqrt(x)/solution.py
@@ -0,0 +1,10 @@
+import math
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        xs = int(math.sqrt(x))
+        return xs
+        
