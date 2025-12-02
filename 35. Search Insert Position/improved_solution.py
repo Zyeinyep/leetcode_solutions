fd48943a01f4dc5053c4fdbc90c90b2c25f1a897
Zyeinyep Khaval <89563758+Zyeinyep@users.noreply.github.com>
2025-12-02 18:48:43 -0500

    Create improved_solution.py

---
+++ 35. Search Insert Position/improved_solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        Time Complexity: O(log n)
+        Space Complexity: O(1)
+        """
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left
