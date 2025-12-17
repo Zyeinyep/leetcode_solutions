579b7fdf52c896efcfd887aa7b55352c2b4a3573
Zyeinyep Khaval <89563758+Zyeinyep@users.noreply.github.com>
2025-12-17 00:03:48 -0500

    Update initial_solution.py

--- 290. Word Pattern/initial_solution.py
+++ 290. Word Pattern/initial_solution.py
@@ -1,1 +1,25 @@
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        """
+        :type pattern: str
+        :type s: str
+        :rtype: bool
+        """
+        s = s.strip().split()
+        map = {}
+        seen = []
+        if len(s) != len(pattern):
+            return False
+        for i,e in enumerate(pattern):
+            if e in map:
+                if map[e] != s[i]:
+                    return False
+            else:
+                if s[i] in seen:
+                    return False
+                map[e] = s[i]
+                seen.append(s[i])
 
+        return True
+       
+            
