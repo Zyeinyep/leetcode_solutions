288dd37d9cd2eaa5531129bfa45ae4261d5c2a2c
Zyeinyep Khaval <89563758+Zyeinyep@users.noreply.github.com>
2025-12-22 19:37:26 -0500

    Update initial_solution.py

--- 189. Rotate Array/initial_solution.py
+++ 189. Rotate Array/initial_solution.py
@@ -1,1 +1,30 @@
 
+from collections import deque
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        last = len(nums) - k
+        arr = nums[:last]
+        arr2 = nums[last:]
+      
+        p = arr2+arr
+
+        for i,e in enumerate(nums):
+            
+            nums[i] = p[i]
+            
+        
+
+
+            
+                
+                    
+        
+                    
+            
+        
