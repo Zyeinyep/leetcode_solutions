95d630a01253f1ec91f93664c1148f5b5f9ea558
Zyeinyep Khaval <89563758+Zyeinyep@users.noreply.github.com>
2025-12-24 15:24:22 -0500

    Update initial_solution.py

--- 15. 3Sum/initial_solution.py
+++ 15. 3Sum/initial_solution.py
@@ -1,1 +1,23 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = set()
+        for i,e in enumerate(nums):
+            target = -e
+            my_dict = {}
+            for j in range(i+1, len(nums)):
+                need = target-nums[j]
+                if need in my_dict:
+                    res.add(tuple(sorted((e,need,nums[j]))))
+                else:
+                    my_dict[nums[j]] = 1
+        return [list(t) for t in res]
 
+            
+
+
+            
+        
