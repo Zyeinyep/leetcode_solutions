ec24cc9a8033d5a061899a1159861992fad9bd93
Zyeinyep Khaval <89563758+Zyeinyep@users.noreply.github.com>
2025-12-30 15:13:41 -0500

    Update improved_solution.py

--- 17. Letter Combinations of a Phone Number/improved_solution.py
+++ 17. Letter Combinations of a Phone Number/improved_solution.py
@@ -1,1 +1,30 @@
 
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        res = []
+        my_dict = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        def backtrack(index, comb):
+            if index == len(digits):
+                res.append("".join(comb))
+                return
+            for i in my_dict[digits[index]]:
+                comb.append(i)
+                backtrack(index+1, comb)
+                comb.pop()
+        backtrack(0,[])
+        return res
+        
