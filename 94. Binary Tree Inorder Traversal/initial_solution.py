e386e7764c978cd237ee13faf484a4eb1d1cc8a2
Zyeinyep Khaval <89563758+Zyeinyep@users.noreply.github.com>
2026-01-13 22:22:25 -0500

    Update initial_solution.py

--- 94. Binary Tree Inorder Traversal/initial_solution.py
+++ 94. Binary Tree Inorder Traversal/initial_solution.py
@@ -1,1 +1,23 @@
-
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        stack = []
+        inorder = []
+        curr = root
+        while stack or curr:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            inorder.append(curr.val)
+            curr = curr.right
+        return inorder
